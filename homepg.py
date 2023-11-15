834b044c95da39d6896a6195aceefa13d7d15c33
Jathin Narayan <jathin04official@gmail.com>
2023-11-15 16:15:09 +0530

    v7.2

--- homepg.py
+++ homepg.py
@@ -1,3 +1,11 @@
 import pickle
 import streamlit as st
 
+# Loading the saved pickle model
+model = pickle.load(open('car_predictor.sav', 'rb'))
+
+st.title('Car Price Predictor')
+
+option = st.selectbox('How would you like to be contacted ? ', ('Email', 'Home phone', 'Mobile phone'))
+
+st.write('You selected: ', option)
